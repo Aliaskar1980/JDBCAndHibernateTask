f1f52baea108774b80138c18318108e9c35bb640
aliaskarguur1980 <aliaskarguur1980@gmail.com>
2021-09-29 21:31:18 +0600

    try catch added

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoHibernateImpl.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoHibernateImpl.java
@@ -2,93 +2,133 @@ package peaksoft.dao;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import peaksoft.model.User;
 import peaksoft.util.HibernateUtil;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private Transaction transaction = null;
 
-    public UserDaoHibernateImpl() throws SQLException {
+    public UserDaoHibernateImpl() {
 
     }
 
     @Override
-    public void createUsersTable() throws SQLException {
-       try{
-           Session session = HibernateUtil.getSessionFactory().openSession();
-           session.beginTransaction();
-           session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
-                           "(id SERIAL PRIMARY KEY ," +
-                           "name VARCHAR(50)," +
-                           "lastName VARCHAR(50)," +
-                           "age INTEGER)").executeUpdate();
+    public void createUsersTable() {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
+                    "(id SERIAL PRIMARY KEY ," +
+                    "name VARCHAR(50)," +
+                    "lastName VARCHAR(50)," +
+                    "age INTEGER)").executeUpdate();
 
-           session.getTransaction().commit();
-           session.close();
-           System.out.println("Created successfully ");
-       } catch (HibernateException e) {
-           e.printStackTrace();
-       }
+            session.getTransaction().commit();
+            session.close();
+            System.out.println("Created successfully ");
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
 
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        session.createSQLQuery("DROP TABLE users").executeUpdate();
-        session.getTransaction().commit();
-        session.close();
-        System.out.println("Successfully deleted ");
 
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE users").executeUpdate();
+            session.getTransaction().commit();
+            session.close();
+            System.out.println("Successfully deleted ");
+        } catch (
+                Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+
+
+            }
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
         Long userId = null;
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        User user = new User(name,lastName,age);
-        userId = (Long) session.save(user);
-        session.getTransaction().commit();
-        session.close();
-        System.out.println(userId + "Успешно добавлено.");
 
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            userId = (Long) session.save(user);
+            session.getTransaction().commit();
+            session.close();
+            System.out.println(userId + "Успешно добавлено.");
+        } catch (
+                Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+
+            }
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        User e = session.get(User.class, id);
-        session.delete(e);
-        session.getTransaction().commit();
-        session.close();
-        System.out.println("Successfully deleted" + e);
-
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            User e = session.get(User.class, id);
+            session.delete(e);
+            session.getTransaction().commit();
+            session.close();
+            System.out.println("Successfully deleted" + e);
+        } catch (
+                Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-    Session session = HibernateUtil.getSessionFactory().openSession();
-    session.beginTransaction();
-    List<User> userList =session.createQuery("FROM User ").getResultList();
-    session.getTransaction().commit();
-    session.close();
-    System.out.println("Found " + userList.size() + " users");
+        List<User> userList = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            userList = session.createQuery("FROM User ").getResultList();
+            session.getTransaction().commit();
+            session.close();
+            System.out.println("Found " + userList.size() + " users");
+
+        } catch (
+                Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
         return userList;
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        session.createQuery("DELETE FROM User").executeUpdate();
-        session.getTransaction().commit();
-        session.close();
-        System.out.println("Successfully deleted all data in User");
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.createQuery("DELETE FROM User").executeUpdate();
+            session.getTransaction().commit();
+            session.close();
+            System.out.println("Successfully deleted all data in User");
 
+        } catch (
+                Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/HibernateUtil.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/HibernateUtil.java
@@ -1,28 +1,43 @@
 package peaksoft.util;
-
-
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import peaksoft.model.User;
+
+import java.util.Properties;
 
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory = buildSessionFactory();
-    private static SessionFactory buildSessionFactory() {
-        try{
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory(){
+        if (sessionFactory==null){
+            try{
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "org.postgresql.Driver");
+                settings.put(Environment.URL,"jdbc:postgresql://localhost:5432/postgres");
+                settings.put(Environment.USER,"postgres");
+                settings.put(Environment.PASS,"oandsandn312");
+                settings.put(Environment.DIALECT,"org.hibernate.dialect.PostgreSQL9Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.HBM2DDL_AUTO, "create");
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
 
-
-            return new Configuration().configure("hibernate.cfg.xml" ).buildSessionFactory();
-        }catch (Throwable ex) {
-            System.out.println("Session not created " + ex);
-            throw new ExceptionInInitializerError(ex);
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-    }
-    public static SessionFactory getSessionFactory() {
         return sessionFactory;
+
     }
     public static void shutDown() {
-
-
-
         getSessionFactory().close();
     }
+
+
 }
